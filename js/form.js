0f372554e812bfadd6f2c50522d7375c5f8965eb
xondemingo <xondemingo@gmail.com>
2026-08-19 14:19:13 +0200

    parse form data

--- js/form.js
+++ js/form.js
@@ -1,8 +1,19 @@
 const form = document.getElementById("lets-talk_form");
 
+function parseFormData(formData) {
+    return {
+        name: formData.get("name").trim(),
+        email: formData.get("email").trim().toLowerCase(),
+        phone: formData.get("phone").trim(),
+        agreesToWhatsApp: formData.has("agreesToWhatsApp"),
+        message: formData.get("message").trim(),
+    }
+}
+
 function submitForm() {
     const formData = new FormData(form);
-    console.log(Object.fromEntries(formData));
+    const parsedFormData = parseFormData(formData);
+    console.log(parsedFormData);
 }
 
 form.addEventListener('submit', (event) => {
